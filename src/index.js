a5f6f9f843371bb179236a0afcca3156c5841847
anton urazgildin <antoha91@mail.ru>
2024-01-28 22:04:19 +0300

    step 2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,50 @@
-import  './styles.scss';
-import  'bootstrap';
+import './styles.scss';
+import 'bootstrap';
+import * as yup from 'yup';
+import onChange from 'on-change';
 
-console.log("Hello World!");
+const state = {
+  state: 'valid',
+  data: '',
+  errors: '',
+  feeds: [],
+};
+
+const schema = yup.string().required().url().notOneOf(state.feeds);
+
+const validate = (data) => {
+  schema.validate(data)
+    .then(() => {
+      state.errors = '';
+    })
+    .catch((err) => {
+      state.errors = err.errors;
+    });
+};
+
+const watchedState = onChange(state, (path, value) => {
+  if (path === 'state') {
+    const input = document.querySelector('.form-control');
+    if (value === 'invalid') {
+      input.classList.add('is-invalid');
+    } else {
+      input.classList.remove('is-invalid');
+    }
+  }
+});
+
+const form = document.querySelector('.rss-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const newUrl = formData.get('url');
+  state.data = newUrl;
+  validate(newUrl);
+  if (state.errors !== '') {
+    watchedState.state = 'invalid';
+  } else {
+    watchedState.state = 'valid';
+    state.feeds.push(newUrl);
+  }
+});
