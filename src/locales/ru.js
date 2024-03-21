a82740fbd10f921d69506dd81054e5f9bef6b0a4
anton urazgildin <antoha91@mail.ru>
2024-03-21 19:07:16 +0300

    project refactoring 3

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -6,6 +6,7 @@ export default {
         ifExist: 'RSS уже существует',
         required: 'Не должно быть пустым',
         notRss: 'Ресурс не содержит валидный RSS',
+        netsError: 'Ошибка сети',
       },
       others: {
         success: 'RSS успешно загружен',
